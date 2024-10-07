841331dbe0b8f922b16a92ab1c8a0dba36fcb6e4
masaaldosey <prabhatreddyk@gmail.com>
2024-10-07 16:45:48 +0200

    feat(include): declare necessary methods

--- include/raveurdf/raveurdf.h
+++ include/raveurdf/raveurdf.h
@@ -7,3 +7,24 @@
 #include <openrave/openrave.h>
 #include <openrave/plugin.h>
 #include <boost/bind/bind.hpp>
+
+#include <memory>
+#include <string>
+
+namespace raveurdf {
+
+// TODO: update docstrings
+class RaveURDF : public OpenRAVE::ModuleBase
+{
+public:
+    RaveURDF(OpenRAVE::EnvironmentBasePtr penv);
+    virtual ~RaveURDF() = default;
+
+    bool loadURDF(std::ostream& soutput, std::istream& sinput);
+    std::string loadModel(urdf::Model& urdf_model, TiXmlDocument& xml_doc, std::string path_to_urdf);
+
+private:
+    // reference to OpenRAVE environment
+    OpenRAVE::EnvironmentBasePtr _env;
+};
+}  // namespace raveurdf
