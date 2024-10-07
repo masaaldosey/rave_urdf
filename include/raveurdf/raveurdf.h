f7e28b2c6bf3806ddff9268eb114060774af91e0
masaaldosey <prabhatreddyk@gmail.com>
2024-10-07 15:03:42 +0200

    fix(include): remove redundant includes

--- include/raveurdf/raveurdf.h
+++ include/raveurdf/raveurdf.h
@@ -1,6 +1,9 @@
 #pragma once
 
+#include <urdf_parser/urdf_parser.h>
+#include <urdf/model.h>
+#include <tinyxml2.h>
+
+#include <openrave/openrave.h>
 #include <openrave/plugin.h>
 #include <boost/bind/bind.hpp>
-using namespace OpenRAVE;
-using namespace boost::placeholders;
